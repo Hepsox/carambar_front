a692b5c7709407202c43d3a1874e494a2da97708
Hepsox <marie.delaire@outlook.com>
2025-06-23 22:13:58 +0200

    fix: update BASE_URL in BlagueService to use the correct production endpoint

--- src/app/service/blagues.service.ts
+++ src/app/service/blagues.service.ts
@@ -5,11 +5,11 @@ import { Blague } from '../type/blague.type';
 
 @Injectable({ providedIn: 'root' })
 export class BlagueService {
-  private BASE_URL = 'http://localhost:3000/blagues/random';
+  private BASE_URL = 'https://carambar-back-1s6g.onrender.com';
 
   constructor(private http: HttpClient) {}
 
   getRandomBlague(): Observable<Blague> {
-    return this.http.get<Blague>(this.BASE_URL);
+    return this.http.get<Blague>(this.BASE_URL + '/blagues/random');
   }
 }
